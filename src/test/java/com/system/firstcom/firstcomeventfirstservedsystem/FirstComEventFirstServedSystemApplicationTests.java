380f441eed918cc5af100826dbbe4bca370813dd
P173073 <deed1515@naver.com>
2024-07-03 09:57:45 +0900

    :tada: [init] 선착순 이벤트 시스템

---
+++ src/test/java/com/system/firstcom/firstcomeventfirstservedsystem/FirstComEventFirstServedSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.firstcom.firstcomeventfirstservedsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstComEventFirstServedSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
